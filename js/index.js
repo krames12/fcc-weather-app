a32c9924b3649cce9a9b6fca9270a59bd55430fd
Nick Kramer <krames12@gmail.com>
2016-02-24 02:25:40 -0500

    Including Javascript file

---
+++ js/index.js
@@ -0,0 +1,69 @@
+console.log("working code");
+var lng;
+var lat;
+var weatherInfo;
+var httpReq = new XMLHttpRequest();
+var request;
+var locationURL;
+var apiKey = "97aa3fe4ed2a80516a95bda9f813a22e";
+var fTemp;
+var cTemp;
+var tempType;
+
+navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+
+function geoError(error){
+	$(".current-city").html("<p>Geolocation is not supported by your browser.");
+}
+
+function geoSuccess(position){
+	lng = position.coords.longitude;
+	lat = position.coords.latitude;
+	locationURL = ("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&APPID=" + apiKey);
+
+	getData(locationURL);
+	console.log(weatherInfo);
+	pageChange();
+}
+
+function getData(loc){
+	httpReq.open("GET", loc, false);
+	httpReq.send();
+	weatherInfo = JSON.parse(httpReq.responseText);
+	fTemp = Math.floor(weatherInfo.main.temp * (9/5) - 459.67);
+	cTemp = Math.floor(weatherInfo.main.temp - 273.15);
+}
+
+function pageChange(){
+	$(".current-city").html(weatherInfo.name.toUpperCase() + ", ");
+	$(".city-country").html(weatherInfo.sys.country.toUpperCase());
+	$(".condition-icon").html("<img src='http://openweathermap.org/img/w/" + weatherInfo.weather[0].icon + ".png' alt='Weather condition icon.'></img>");
+	$(".weather-condition").html(weatherInfo.weather[0].description.toUpperCase());
+
+	if(weatherInfo.sys.country == "US"){
+		tempType = "f";
+		$(".temp-number").html(fTemp);
+		$(".temp-unit").html("&#8457");
+	} else {
+		tempType = "c"
+		$(".temp-number").html(cTemp);
+		$(".temp-unit").html("&#8451");
+	}
+}
+
+$(".temp-unit").click(function(){
+	if(tempType == "c"){
+		tempType = "f";
+		$(".temp-number").html(fTemp);
+		$(".temp-unit").html("&#8457");
+	} else if (tempType == "f") {
+		tempType = "c"
+		$(".temp-number").html(cTemp);
+		$(".temp-unit").html("&#8451");
+	}
+})
+
+$(".refresh").click(function(){
+	navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+	console.log("refreshed");
+})
